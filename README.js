384e96b6dbb2e2bba27da2209079a0cbb3f22f0e
hakolao <okkohakola@gmail.com>
2020-05-06 12:16:31 +0300

    Init

---
+++ README.js
@@ -0,0 +1,33 @@
+# Speech To Text
+
+
+## Usage
+```
+git clone git@github.com:Artemso/BTTLR_hackathon.git bttlr_hackathon
+cd bttlr_hackathon
+cp credentials_example.json credentials.json
+# Fill credentials (request from team)
+```
+
+### Before running the app
+
+#### MacOS install sox & node
+```
+brew install sox
+# if you don't have node installed, do these
+# brew install nvm
+# nvm install node
+# nvm use node
+```
+
+#### Linux install sox & node
+```
+# Install NVM https://github.com/nvm-sh/nvm#install--update-script
+nvm install node
+nvm use node
+```
+
+```
+npm install
+node index.js
+```
